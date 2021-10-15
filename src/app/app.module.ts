e79ee257a91412dde120cc69b9cc1552d5ccb3e9
Steven Wilde <wyld16steven@gmail.com>
2021-10-15 16:23:46 -0500

    Perform the search and display the results

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
@@ -10,7 +11,8 @@ import { SearchComponent } from './search/search.component';
     SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  users: Array<any> = [];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  searchUsers(query: string) {
+    this.http.get('https://api.stackexchange.com/2.3/users?order=desc&sort=name&inname=' + query + '&site=stackoverflow')
+      .subscribe(list => this.updateList(list))
+    return;
+  }
+
+  updateList(list: any) {
+    this.users = list.items;
   }
 
 }
